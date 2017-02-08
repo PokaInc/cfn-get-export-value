51b23846e63011bfa824ad2e139303074f99d7df
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2017-02-08 15:38:27 -0800

    v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='cfn_get_export_value',
     packages=['cfn_get_export_value'],  # this must be the same as the name above
-    version='0.0.1',
+    version='0.0.2',
     description='Get an exported value in AWS CloudFormation',
     author='Simon-Pierre Gingras',
     author_email='spgingras@poka.io',
