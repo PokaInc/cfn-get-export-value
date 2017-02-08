8f1b4829d11ad2a7e9b1b03b533669fd8318423d
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2017-02-08 10:29:15 -0800

    reformat

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
+
 setup(
-  name = 'cfn_get_export_value',
-  packages = ['cfn_get_export_value'], # this must be the same as the name above
-  version = '0.0.1',
-  description = 'Get an exported value in AWS CloudFormation',
-  author = 'Simon-Pierre Gingras',
-  author_email = 'spgingras@poka.io',
-  url = 'https://github.com/PokaInc/cfn-get-export-value', # use the URL to the github repo
-  download_url = 'https://github.com/PokaInc/cfn-get-export-value/tarball/0.0.1',
-  keywords = ['aws', 'cloudformation', 'exports'], # arbitrary keywords
-  classifiers = [],
+    name='cfn_get_export_value',
+    packages=['cfn_get_export_value'],  # this must be the same as the name above
+    version='0.0.1',
+    description='Get an exported value in AWS CloudFormation',
+    author='Simon-Pierre Gingras',
+    author_email='spgingras@poka.io',
+    url='https://github.com/PokaInc/cfn-get-export-value',  # use the URL to the github repo
+    download_url='https://github.com/PokaInc/cfn-get-export-value/tarball/0.0.1',
+    keywords=['aws', 'cloudformation', 'exports'],  # arbitrary keywords
+    classifiers=[],
 )
